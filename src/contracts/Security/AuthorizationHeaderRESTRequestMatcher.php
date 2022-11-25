fce7bb9b56fde767f96e71803ca0c151f39c270c
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2022-11-25 11:45:32 +0100

    IBX-4359: Added dedicated service for token-based authentication

---
+++ src/contracts/Security/AuthorizationHeaderRESTRequestMatcher.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Contracts\Rest\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestMatcher;
+
+final class AuthorizationHeaderRESTRequestMatcher extends RequestMatcher
+{
+    public function matches(Request $request): bool
+    {
+        if ($request->attributes->get('is_rest_request', false) !== true) {
+            return false;
+        }
+
+        if ($request->attributes->get('_route') !== 'ibexa.rest.create_token'
+            && !$request->headers->has('Authorization')) {
+            return false;
+        }
+
+        return parent::matches($request);
+    }
+}
