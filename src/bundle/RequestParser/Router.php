079c5c561e6897f68e604a0a71f05916931a3346
Andrew Longosz <alongosz@users.noreply.github.com>
2023-07-25 16:42:37 +0200

    Fixed Router-based Request Parser to rely on URI Parser for BC

--- src/bundle/RequestParser/Router.php
+++ src/bundle/RequestParser/Router.php
@@ -6,55 +6,37 @@
  */
 namespace Ibexa\Bundle\Rest\RequestParser;
 
-use Ibexa\Contracts\Rest\Exceptions\InvalidArgumentException;
+use Ibexa\Contracts\Rest\UriParser\UriParserInterface;
 use Ibexa\Rest\RequestParser;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\RouterInterface;
 
 /**
+ * @deprecated use \Ibexa\Contracts\Rest\UriParser\UriParserInterface instead
+ * @see \Ibexa\Contracts\Rest\UriParser\UriParserInterface
+ *
  * Router based request parser.
  */
 class Router implements RequestParser
 {
-    /**
-     * @var \Symfony\Cmf\Component\Routing\ChainRouter
-     */
-    private $router;
+    private RouterInterface $router;
 
-    public function __construct(RouterInterface $router)
+    private UriParserInterface $uriParser;
+
+    public function __construct(RouterInterface $router, UriParserInterface $uriParser)
     {
         $this->router = $router;
+        $this->uriParser = $uriParser;
     }
 
     /**
-     * @throws \Symfony\Component\Routing\Exception\ResourceNotFoundException If no match was found
+     * @return array<mixed> matched route configuration and parameters
+     *
+     * @throws \Ibexa\Contracts\Rest\Exceptions\InvalidArgumentException If no match was found
      */
-    public function parse($url)
+    public function parse($url): array
     {
-        // we create a request with a new context in order to match $url to a route and get its properties
-        $request = Request::create($url, 'GET');
-        $originalContext = $this->router->getContext();
-        $context = clone $originalContext;
-        $context->fromRequest($request);
-        $this->router->setContext($context);
-
-        try {
-            $matchResult = $this->router->matchRequest($request);
-        } catch (ResourceNotFoundException $e) {
-            // Note: this probably won't occur in real life because of the legacy matcher
-            $this->router->setContext($originalContext);
-            throw new InvalidArgumentException("No route matched '$url'");
-        }
-
-        if (!$this->matchesRestRequest($matchResult)) {
-            $this->router->setContext($originalContext);
-            throw new InvalidArgumentException("No route matched '$url'");
-        }
-
-        $this->router->setContext($originalContext);
-
-        return $matchResult;
+        return $this->uriParser->matchUri($url);
     }
 
     public function generate($type, array $values = [])
@@ -63,35 +45,11 @@ class Router implements RequestParser
     }
 
     /**
-     * @throws \Ibexa\Core\Base\Exceptions\InvalidArgumentException If $attribute wasn't found in the match
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException If $attribute wasn't found in the match
      */
     public function parseHref($href, $attribute)
     {
-        $parsingResult = $this->parse($href);
-
-        if (!isset($parsingResult[$attribute])) {
-            throw new InvalidArgumentException("No attribute '$attribute' in route matched from $href");
-        }
-
-        return $parsingResult[$attribute];
-    }
-
-    /**
-     * Checks if a router match response matches a REST resource.
-     *
-     * @param array $match Match array returned by Router::match() / Router::matchRequest()
-     *
-     * @throws \Ibexa\Contracts\Rest\Exceptions\InvalidArgumentException if the \$match isn't valid
-     *
-     * @return bool
-     */
-    private function matchesRestRequest(array $match)
-    {
-        if (!isset($match['_route'])) {
-            throw new InvalidArgumentException('Invalid $match parameter, no _route key');
-        }
-
-        return strpos($match['_route'], 'ibexa.rest.') === 0;
+        return $this->uriParser->getAttributeFromUri($href, $attribute);
     }
 }
 
