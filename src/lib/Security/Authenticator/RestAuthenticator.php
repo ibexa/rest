fb8431c36e0b36e60359718cfe33f798b712bc84
konradoboza <konrad.oboza@ibexa.co>
2024-05-28 14:18:59 +0200

    IBX-8290: Reworked REST authentication to comply with the new Symfony authenticator mechanism under separate firewall

--- src/lib/Security/Authenticator/RestAuthenticator.php
+++ src/lib/Security/Authenticator/RestAuthenticator.php
@@ -13,6 +13,7 @@ use Ibexa\Rest\Input\Dispatcher;
 use Ibexa\Rest\Message;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
@@ -22,20 +23,40 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 final class RestAuthenticator extends AbstractAuthenticator
 {
+    private const string ACCEPT_HEADER = 'Accept';
+    private const string SESSION_HEADER_VALUE = 'application/vnd.ibexa.api.Session';
+
     public function __construct(
         private readonly Dispatcher $inputDispatcher,
+        private readonly TokenStorageInterface $tokenStorage,
     ) {
     }
 
     public function supports(Request $request): ?bool
     {
         return
-            $request->headers->has('Accept') &&
-            str_contains($request->headers->get('Accept') ?? '', 'application/vnd.ibexa.api.Session');
+            $request->headers->has(self::ACCEPT_HEADER) &&
+            str_contains(
+                $request->headers->get(self::ACCEPT_HEADER) ?? '',
+                self::SESSION_HEADER_VALUE
+            );
     }
 
     public function authenticate(Request $request): Passport
     {
+        $existingUserToken = $this->fetchExistingToken($request);
+        if ($this->canUserFromSessionBeAuthenticated($existingUserToken)) {
+            /** @phpstan-ignore-next-line */
+            $existingUser = $existingUserToken->getUser();
+
+            return $this->createAuthorizationPassport(
+                /** @phpstan-ignore-next-line */
+                $existingUser->getUserIdentifier(),
+                /** @phpstan-ignore-next-line */
+                $existingUser->getPassword()
+            );
+        }
+
         /** @var \Ibexa\Rest\Server\Values\SessionInput $sessionInput */
         $sessionInput = $this->inputDispatcher->parse(
             new Message(
@@ -50,10 +71,7 @@ final class RestAuthenticator extends AbstractAuthenticator
         $request->attributes->set('username', $login);
         $request->attributes->set('password', $password);
 
-        return new Passport(
-            new UserBadge($sessionInput->login),
-            new PasswordCredentials($password),
-        );
+        return $this->createAuthorizationPassport($login, $password);
     }
 
     public function onAuthenticationSuccess(
@@ -70,5 +88,44 @@ final class RestAuthenticator extends AbstractAuthenticator
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         throw new UnauthorizedException($exception->getMessage());
+    }
+
+    private function fetchExistingToken(Request $request): ?TokenInterface
+    {
+        // If a token already exists and username is the same as the one we request authentication for,
+        // then return it and mark it as coming from session.
+        $previousToken = $this->tokenStorage->getToken();
+        if (
+            $previousToken === null ||
+            $previousToken->getUsername() !== $request->attributes->get('username')
+        ) {
+            return null;
+        }
+
+        $previousToken->setAttribute('isFromSession', true);
+
+        return $previousToken;
+    }
+
+    private function canUserFromSessionBeAuthenticated(?TokenInterface $existingUserToken): bool
+    {
+        if ($existingUserToken === null) {
+            return false;
+        }
+
+        $user = $existingUserToken->getUser();
+        if ($user === null || $user->getPassword() === null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function createAuthorizationPassport(string $login, string $password): Passport
+    {
+        return new Passport(
+            new UserBadge($login),
+            new PasswordCredentials($password),
+        );
     }
 }
