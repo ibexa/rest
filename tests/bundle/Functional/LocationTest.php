5c8cfd1fbfc8faa9b7594c853094920ac8974a1d
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-06-07 16:57:57 +0200

    IBX-8180: Added integration test

--- tests/bundle/Functional/LocationTest.php
+++ tests/bundle/Functional/LocationTest.php
@@ -333,4 +333,23 @@ XML;
 
         self::assertHttpResponseCodeEquals($response, 204);
     }
+
+    /**
+     * @depends testMoveLocation
+     */
+    public function testSwap(string $locationHref): void
+    {
+        $request = $this->createHttpRequest(
+            'POST',
+            $locationHref,
+            'SwapLocationInput+json',
+            '',
+            json_encode(['SwapLocationInput' => ['destination' => '/1/5']]) ?: '',
+        );
+
+        $response = $this->sendHttpRequest($request);
+
+        self::assertHttpResponseCodeEquals($response, 204);
+        self::assertHttpResponseHasHeader($response, 'Location');
+    }
 }
