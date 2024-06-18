77b2bf5afabfe1f49183607ead63745e73f5b2a9
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-06-18 18:53:23 +0200

    IBX-8176: Refactored solution

--- tests/bundle/Routing/ExpressionLanguage/ContentTypeHeaderMatcherFactoryTest.php
+++ tests/bundle/Routing/ExpressionLanguage/ContentTypeHeaderMatcherFactoryTest.php
@@ -10,27 +10,23 @@ namespace Ibexa\Tests\Bundle\Rest\Routing\ExpressionLanguage;
 
 use Closure;
 use Ibexa\Bundle\Rest\Routing\ExpressionLanguage\ContentTypeHeaderMatcherFactory;
-use Ibexa\Contracts\Rest\Input\ParsingDispatcher;
-use PHPUnit\Framework\MockObject\MockObject;
+use Ibexa\Contracts\Rest\Input\MediaTypeParser;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ContentTypeHeaderMatcherFactoryTest extends TestCase
 {
-    private readonly ParsingDispatcher&MockObject $parsingDispatcher;
-
     private readonly Closure $contentTypeHeaderMatcher;
 
     protected function setUp(): void
     {
-        parent::setUp();
-
-        $this->parsingDispatcher = $this->createMock(ParsingDispatcher::class);
-        $contentTypeHeaderMatcherFactory = new ContentTypeHeaderMatcherFactory($this->parsingDispatcher);
+        $contentTypeHeaderMatcherFactory = new ContentTypeHeaderMatcherFactory(
+            new MediaTypeParser(),
+        );
         $this->contentTypeHeaderMatcher = $contentTypeHeaderMatcherFactory();
     }
 
-    public function testRequestContentTypeMatchesRoute(): void
+    public function testGetMediaType(): void
     {
         $request = new Request();
         $request->headers->add([
@@ -38,29 +34,19 @@ final class ContentTypeHeaderMatcherFactoryTest extends TestCase
         ]);
 
         $closure = $this->contentTypeHeaderMatcher;
-        $this->mockParsingDispatcher('application/vnd.ibexa.api.CopyContentTypeInput');
 
-        self::assertTrue($closure($request, 'application/vnd.ibexa.api.CopyContentTypeInput'));
+        self::assertSame('CopyContentTypeInput', $closure($request));
     }
 
     public function testRequestContentTypeDoesNotMatchRoute(): void
     {
         $request = new Request();
         $request->headers->add([
-            'Content-Type' => 'application/vnd.ibexa.api.CreateContentTypeInput+xml',
+            'Content-Type' => 'application.CreateContentTypeInput+xml',
         ]);
 
         $closure = $this->contentTypeHeaderMatcher;
-        $this->mockParsingDispatcher('application/vnd.ibexa.api.CreateContentTypeInput');
 
-        self::assertFalse($closure($request, 'application/vnd.ibexa.api.CopyContentTypeInput'));
-    }
-
-    private function mockParsingDispatcher(string $returnedMediaType): void
-    {
-        $this->parsingDispatcher
-            ->expects(self::once())
-            ->method('fetchMediaTypeWithoutVersion')
-            ->willReturn($returnedMediaType);
+        self::assertNull($closure($request));
     }
 }
