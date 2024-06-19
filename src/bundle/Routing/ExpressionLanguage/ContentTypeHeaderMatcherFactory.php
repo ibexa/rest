b08d3d521a9197da345e73312d69d211cc5b6a1e
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-06-19 12:53:52 +0200

    IBX-8176: Refactoring

--- src/bundle/Routing/ExpressionLanguage/ContentTypeHeaderMatcherFactory.php
+++ src/bundle/Routing/ExpressionLanguage/ContentTypeHeaderMatcherFactory.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 
 namespace Ibexa\Bundle\Rest\Routing\ExpressionLanguage;
 
-use Closure;
 use Ibexa\Contracts\Rest\Input\MediaTypeParserInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -19,16 +18,13 @@ final readonly class ContentTypeHeaderMatcherFactory
     ) {
     }
 
-    public function __invoke(): Closure
+    public function __invoke(Request $request): ?string
     {
-        return function (Request $request): ?string {
-            $contentTypeHeaderValue = $request->headers->get('Content-Type');
+        $contentTypeHeaderValue = $request->headers->get('Content-Type');
+        if ($contentTypeHeaderValue === null) {
+            return null;
+        }
 
-            if ($contentTypeHeaderValue === null) {
-                return null;
-            }
-
-            return $this->mediaTypeParser->parseContentTypeHeader($contentTypeHeaderValue);
-        };
+        return $this->mediaTypeParser->parseContentTypeHeader($contentTypeHeaderValue);
     }
 }
