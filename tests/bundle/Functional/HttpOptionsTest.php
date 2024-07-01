4da63c8fc32aef50e7c83667171e0151a5559a92
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-07-01 11:04:34 +0200

    IBX-8173: OpenAPI compatible content type copy endpoint

--- tests/bundle/Functional/HttpOptionsTest.php
+++ tests/bundle/Functional/HttpOptionsTest.php
@@ -84,6 +84,7 @@ class HttpOptionsTest extends TestCase
             ['/content/typegroups/1', ['GET', 'PATCH', 'DELETE']],
             ['/content/typegroups/1/types', ['GET', 'POST']],
             ['/content/types', ['GET']],
+            ['/content/types/1', ['POST'], 'CopyContentTypeInput+json'],
             ['/content/types/1', ['COPY', 'GET', 'POST', 'DELETE']],
             ['/content/types/1/draft', ['DELETE', 'GET', 'PATCH', 'PUBLISH']],
             ['/content/types/1/fieldDefinitions', ['GET']],
