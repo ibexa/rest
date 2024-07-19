db281fa9568be29b937f3b1f0095e2ae75b5753a
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2024-07-19 09:07:09 +0200

    IBX-8597: Added BaseSortClauseProcessor
    https://issues.ibexa.co/browse/IBX-8597

---
+++ src/contracts/Input/Parser/Query/SortClause/BaseSortClauseProcessor.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\Contracts\Rest\Input\Parser\Query\SortClause;
+
+use Ibexa\Contracts\Rest\Exceptions;
+use Ibexa\Contracts\Rest\Input\ParsingDispatcher;
+use Traversable;
+
+/**
+ * @template TSortClause
+ *
+ * @internal
+ */
+abstract class BaseSortClauseProcessor implements SortClauseProcessorInterface
+{
+    private ParsingDispatcher $parsingDispatcher;
+
+    public function __construct(ParsingDispatcher $parsingDispatcher)
+    {
+        $this->parsingDispatcher = $parsingDispatcher;
+    }
+
+    public function processSortClauses(array $sortClauseData): Traversable
+    {
+        if (empty($sortClauseData)) {
+            yield from [];
+        }
+
+        foreach ($sortClauseData as $sortClauseName => $direction) {
+            $mediaType = $this->getSortClauseMediaType($sortClauseName);
+
+            try {
+                yield $this->parsingDispatcher->parse([$sortClauseName => $direction], $mediaType);
+            } catch (Exceptions\Parser $e) {
+                throw new Exceptions\Parser($this->getParserInvalidSortClauseMessage($sortClauseName), 0, $e);
+            }
+        }
+    }
+
+    abstract protected function getMediaTypePrefix(): string;
+
+    abstract protected function getParserInvalidSortClauseMessage(string $sortClauseName): string;
+    
+    private function getSortClauseMediaType(string $sortClauseName): string
+    {
+        $mediaTypePrefix = $this->getMediaTypePrefix();
+        if ('.' !== substr($mediaTypePrefix, strlen($mediaTypePrefix) - 1)) {
+            $mediaTypePrefix .= '.';
+        }
+
+        return $mediaTypePrefix . $sortClauseName;
+    }
+}
