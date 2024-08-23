fa4084cb114a6131baa821263f3274b12be2c04c
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-08-23 13:33:17 +0200

    Remove `ValueObjectVisitorDispatcher`

---
+++ src/contracts/Output/ValueObjectVisitorResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Contracts\Rest\Output;
+
+final class ValueObjectVisitorResolver implements ValueObjectVisitorResolverInterface
+{
+    /**
+     * @var array<class-string, ValueObjectVisitor>
+     */
+    private array $visitors;
+
+    /**
+     * @param class-string $visitedClassName
+     */
+    public function addVisitor(string $visitedClassName, $visitor): void
+    {
+        $this->visitors[$visitedClassName] = $visitor;
+    }
+
+    public function resolveValueObjectVisitor(object $object): ?ValueObjectVisitor
+    {
+        $className = $object::class;
+
+        do {
+            if (isset($this->visitors[$className])) {
+                return $this->visitors[$className];
+            }
+        } while ($className = get_parent_class($className));
+
+        return null;
+    }
+}
