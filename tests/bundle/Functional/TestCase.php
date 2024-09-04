7f00b8d4e2d2bcf67ab380a1f4dc3447800452c3
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-09-04 16:45:14 +0200

    Rollback

--- tests/bundle/Functional/TestCase.php
+++ tests/bundle/Functional/TestCase.php
@@ -399,7 +399,7 @@ XML;
     {
         $request = $this->createAuthenticationHttpRequest($this->getLoginUsername(), $this->getLoginPassword());
         $response = $this->sendHttpRequest($request);
-        self::assertHttpResponseCodeEquals($response, 200); //TODO is 200 correct here?
+        self::assertHttpResponseCodeEquals($response, 201);
 
         return json_decode($response->getBody()->getContents(), false, JSON_THROW_ON_ERROR)->Session;
     }
