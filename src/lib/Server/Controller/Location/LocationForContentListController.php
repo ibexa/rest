1aa05c3bcfc907b335c2049823b22a260b9fe98e
tischsoic <symfiz@gmail.com>
2024-09-17 15:28:19 +0200

    add missing Api Platform attributes in Location/ and ObjectState/

--- src/lib/Server/Controller/Location/LocationForContentListController.php
+++ src/lib/Server/Controller/Location/LocationForContentListController.php
@@ -10,6 +10,7 @@ namespace Ibexa\Rest\Server\Controller\Location;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use ApiPlatform\OpenApi\Factory\OpenApiFactory;
 use ApiPlatform\OpenApi\Model;
 use Ibexa\Contracts\Core\Repository\ContentService;
@@ -27,6 +28,68 @@ use JMS\TranslationBundle\Annotation\Ignore;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Get(
+    uriTemplate: '/content/objects/{contentId}/locations',
+    name: 'Get Locations for content item',
+    openapi: new Model\Operation(
+        summary: 'Loads all Locations for the given content item.',
+        tags: [
+            'Objects',
+        ],
+        parameters: [
+            new Model\Parameter(
+                name: 'Accept',
+                in: 'header',
+                required: true,
+                description: 'If set, the Location list is returned in XML or JSON format.',
+                schema: [
+                    'type' => 'string',
+                ],
+            ),
+            new Model\Parameter(
+                name: 'If-None-Match',
+                in: 'header',
+                required: true,
+                description: 'ETag',
+                schema: [
+                    'type' => 'string',
+                ],
+            ),
+            new Model\Parameter(
+                name: 'contentId',
+                in: 'path',
+                required: true,
+                schema: [
+                    'type' => 'string',
+                ],
+            ),
+        ],
+        responses: [
+            Response::HTTP_OK => [
+                'content' => [
+                    'application/vnd.ibexa.api.LocationList+xml' => [
+                        'schema' => [
+                            '$ref' => '#/components/schemas/LocationList',
+                        ],
+                        'x-ibexa-example-file' => '@IbexaRestBundle/Resources/api_platform/examples/content/objects/content_id/locations/GET/LocationList.xml.example',
+                    ],
+                    'application/vnd.ibexa.api.LocationList+json' => [
+                        'schema' => [
+                            '$ref' => '#/components/schemas/LocationListWrapper',
+                        ],
+                        'x-ibexa-example-file' => '@IbexaRestBundle/Resources/api_platform/examples/content/objects/content_id/locations/GET/LocationList.json.example',
+                    ],
+                ],
+            ],
+            Response::HTTP_UNAUTHORIZED => [
+                'description' => 'Error - the user is not authorized to read this content item.',
+            ],
+            Response::HTTP_NOT_FOUND => [
+                'description' => 'Error - the content item with the given ID does not exist.',
+            ],
+        ],
+    ),
+)]
 class LocationForContentListController extends LocationBaseController
 {
     /**
