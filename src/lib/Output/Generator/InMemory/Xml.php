5cf488f3164b94c9efcb8d518394b10113c25582
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-10-01 16:28:31 +0200

    Make unit tests use the new XML generator

--- src/lib/Output/Generator/InMemory/Xml.php
+++ src/lib/Output/Generator/InMemory/Xml.php
@@ -10,6 +10,8 @@ namespace Ibexa\Rest\Output\Generator\InMemory;
 
 use Ibexa\Rest\Output\Generator\Json;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 final class Xml extends Json
 {
@@ -27,6 +29,11 @@ final class Xml extends Json
         $this->checkStartAttribute($name);
 
         $this->json->{'@' . $name} = $value;
+    }
+
+    public function serializeBool($boolValue)
+    {
+        return $boolValue ? 'true' : 'false';
     }
 
     public function startValueElement(string $name, $value, array $attributes = []): void
@@ -51,6 +58,26 @@ final class Xml extends Json
         }
     }
 
+    /**
+     * End document.
+     *
+     * Returns the generated document as a string.
+     */
+    public function endDocument(mixed $data): string
+    {
+        parent::endDocument($data);
+
+        $normalizedData = $this->toArray();
+
+        $encoderContext = $this->getEncoderContext($normalizedData);
+        $encoderContext['as_collection'] = true;
+        $transformedData = $this->transformData($normalizedData);
+
+        $serializer = new Serializer([new ObjectNormalizer()], [new XmlEncoder()]);
+
+        return $serializer->encode($transformedData, 'xml', $encoderContext);
+    }
+
     public function transformData(array $normalizedData): array
     {
         $topNodeName = array_key_first($normalizedData);
@@ -64,28 +91,30 @@ final class Xml extends Json
             $data['#'] = $normalizedData[$topNodeName];
         }
 
-        return $this->clearEmptyArrays($data);
+        return $data;
     }
 
-    /**
-     * @param array<mixed> $array
-     *
-     * @return array<mixed>
-     */
-    private function clearEmptyArrays(array &$array): array
-    {
-        foreach ($array as $key => $value) {
-            if (is_array($value)) {
-                $array[$key] = $this->clearEmptyArrays($value);
-
-                if (empty($array[$key])) {
-                    unset($array[$key]);
-                }
-            }
-        }
-
-        return $array;
-    }
+//    /**
+//     * @param array<mixed> $array
+//     *
+//     * @return array<mixed>
+//     */
+//    private function clearEmptyArrays(array &$array): array
+//    {
+//        foreach ($array as $key => &$value) {
+//            if (is_array($value)) {
+//                // Recursively apply the function to the nested array
+//                $this->clearEmptyArrays($value);
+//
+//                // Remove the field if it's an empty array after recursion
+//                if (empty($value)) {
+//                    unset($array[$key]);
+//                }
+//            }
+//        }
+//
+//        return $array;
+//    }
 
     public function getEncoderContext(array $data): array
     {
