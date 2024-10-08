0974a7339aeaca64f3d33c15aa9aad092cece8d6
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2024-10-08 09:34:01 +0200

    Introduced normalizers for Generator state

--- src/lib/Output/Normalizer/JsonObjectNormalizer.php
+++ src/lib/Output/Normalizer/JsonObjectNormalizer.php
@@ -1,12 +1,21 @@
 <?php
 
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
 namespace Ibexa\Rest\Output\Normalizer;
 
 use Ibexa\Rest\Output\Generator\Json\JsonObject;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-final class JsonObjectNormalizer implements NormalizerInterface
+final class JsonObjectNormalizer implements NormalizerInterface, NormalizerAwareInterface
 {
+    use NormalizerAwareTrait;
+
     /**
      * @param array<mixed> $context
      *
@@ -14,7 +23,15 @@ final class JsonObjectNormalizer implements NormalizerInterface
      */
     public function normalize($object, ?string $format = null, array $context = []): array
     {
-        return get_object_vars($object);
+        $vars = get_object_vars($object);
+
+        unset($vars['_ref_parent']);
+
+        foreach ($vars as $name => $value) {
+            $vars[$name] = $this->normalizer->normalize($value, $format, $context);
+        }
+
+        return $vars;
     }
 
     public function supportsNormalization($data, ?string $format = null): bool
