9026db0c1f8245c5e94bb5c6a7ff5c993d88d0e2
Sztig <sebastianjamula@gmail.com>
2025-03-12 16:27:51 +0100

    added tests for the array return with a dummy "MoneyObject"

---
+++ tests/lib/Output/Normalizer/ArrayNormalizerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace Ibexa\Tests\Rest\Output\Normalizer;
+
+use Ibexa\Rest\Output\Normalizer\JsonSerializableNormalizer;
+use Ibexa\Tests\Rest\Output\MoneyObject;
+use PHPUnit\Framework\TestCase;
+
+class ArrayNormalizerTest extends TestCase
+{
+    public function testNormalizeArray(): void
+    {
+        $normalizer = new JsonSerializableNormalizer();
+        $money = new MoneyObject(100, 'EUR');
+
+        $result = $normalizer->normalize($money);
+
+        self::assertSame('100', $result['amount']);
+        self::assertSame('EUR', $result['currency']);
+    }
+}
