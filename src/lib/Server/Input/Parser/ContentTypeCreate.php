269fa248a39b51776859d843cf2ca0cef7f890fb
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2025-06-09 14:45:00 +0200

    PHPStan

--- src/lib/Server/Input/Parser/ContentTypeCreate.php
+++ src/lib/Server/Input/Parser/ContentTypeCreate.php
@@ -133,7 +133,7 @@ class ContentTypeCreate extends BaseParser
                 throw new Exceptions\Parser("Missing '_href' attribute for the User element in ContentTypeCreate.");
             }
 
-            $contentTypeCreateStruct->creatorId = $this->uriParser->getAttributeFromUri($data['User']['_href'], 'userId');
+            $contentTypeCreateStruct->creatorId = (int)$this->uriParser->getAttributeFromUri($data['User']['_href'], 'userId');
         }
 
         if (!array_key_exists('FieldDefinitions', $data)) {
