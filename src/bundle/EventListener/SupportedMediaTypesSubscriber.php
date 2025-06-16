2144d0d9c3cc55fdb02970d869748963046d02b4
konradoboza <konrad.oboza@ibexa.co>
2025-06-16 15:26:59 +0200

    cr remarks 3

--- src/bundle/EventListener/SupportedMediaTypesSubscriber.php
+++ src/bundle/EventListener/SupportedMediaTypesSubscriber.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Ibexa\Bundle\Rest\EventListener;
 
+use RuntimeException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
@@ -39,16 +40,24 @@ final class SupportedMediaTypesSubscriber implements EventSubscriberInterface
             return;
         }
 
+        $contentTypeHeader = $request->headers->get('Content-Type') ?? '';
         $acceptHeader = $request->headers->get('Accept') ?? '';
 
-        preg_match(self::SUPPORTED_MEDIA_TYPES_REGEX, $acceptHeader, $matches);
+        try {
+            $isContentTypeHeaderSupported = $this->isMediaTypeSupported(
+                $contentTypeHeader,
+                $supportedMediaTypes
+            );
 
-        $match = reset($matches);
-        if ($match === false) {
-            return;
-        }
+            $isAcceptHeaderSupported = $this->isMediaTypeSupported(
+                $acceptHeader,
+                $supportedMediaTypes
+            );
 
-        if (in_array($match, $supportedMediaTypes, true)) {
+            if ($isContentTypeHeaderSupported && $isAcceptHeaderSupported) {
+                return;
+            }
+        } catch (RuntimeException $e) {
             return;
         }
 
@@ -60,5 +69,25 @@ final class SupportedMediaTypesSubscriber implements EventSubscriberInterface
             null,
             Response::HTTP_UNSUPPORTED_MEDIA_TYPE
         );
+    }
+
+    /**
+     * @param string[] $supportedMediaTypes
+     */
+    private function isMediaTypeSupported(
+        string $header,
+        array $supportedMediaTypes
+    ): bool {
+        preg_match(self::SUPPORTED_MEDIA_TYPES_REGEX, $header, $matches);
+
+        $match = reset($matches);
+        if ($match === false) {
+            throw new RuntimeException(sprintf(
+                'Failed to extract media type from header: %s',
+                $header
+            ));
+        }
+
+        return in_array($match, $supportedMediaTypes, true);
     }
 }
