fb3e879e8a7e1d26cb1e3eca0e1e08b2af5260b7
Konrad Oboza <konrad.oboza@ibexa.co>
2025-08-06 14:47:08 +0200

    IBX-10144: Implemented `supported_media_types` flag for REST requests (#179)
    * IBX-10144: Implemented `xml_disabled` flag for REST requests
    
    * reworked the solution to have a whitelist of formats instead
    
    * added additional test for unknown header/media-type
    
    * cr remarks
    
    * cr remarks 2
    
    * cr remarks 3
    
    * regenerated baseline
    
    * regenerated baseline
    
    * regenerated baseline

---
+++ tests/bundle/EventListener/SupportedMediaTypesSubscriberTest.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\Rest\EventListener;
+
+use Ibexa\Bundle\Rest\EventListener\SupportedMediaTypesSubscriber;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+final class SupportedMediaTypesSubscriberTest extends TestCase
+{
+    /** @var \Symfony\Component\HttpKernel\HttpKernelInterface&\PHPUnit\Framework\MockObject\MockObject */
+    private HttpKernelInterface $kernel;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->kernel = $this->createMock(HttpKernelInterface::class);
+    }
+
+    public function testDoesNothingWhenSupportedMediaTypesParameterIsNotSet(): void
+    {
+        $request = new Request();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $subscriber->allowOnlySupportedMediaTypes($event);
+
+        self::expectNotToPerformAssertions();
+    }
+
+    public function testDoesNothingWhenSupportedMediaTypesParameterIsEmpty(): void
+    {
+        $request = new Request();
+        $request->attributes->set('supported_media_types', []);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $subscriber->allowOnlySupportedMediaTypes($event);
+        self::expectNotToPerformAssertions();
+    }
+
+    public function testDoesNothingWhenMediaTypeIsSupported(): void
+    {
+        $request = new Request();
+        $request->attributes->set('supported_media_types', ['json', 'xml']);
+        $request->headers = new HeaderBag([
+            'Content-Type' => 'application/vnd.ibexa.api.ContentCreate+json',
+            'Accept' => 'application/vnd.ibexa.api.ContentCreate+json',
+        ]);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $subscriber->allowOnlySupportedMediaTypes($event);
+
+        self::expectNotToPerformAssertions();
+    }
+
+    public function testThrowsExceptionWhenHeaderTypeIsNotSupported(): void
+    {
+        $request = new Request();
+        $request->attributes->set('supported_media_types', ['json']);
+        $request->headers = new HeaderBag([
+            'Content-Type' => 'application/vnd.ibexa.api.ContentCreate+xml',
+            'Accept' => 'application/vnd.ibexa.api.ContentCreate+xml',
+        ]);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $this->expectException(UnsupportedMediaTypeHttpException::class);
+        $subscriber->allowOnlySupportedMediaTypes($event);
+    }
+
+    public function testThrowsExceptionWhenUnknownMediaTypeIsUsed(): void
+    {
+        $request = new Request();
+        $request->attributes->set('supported_media_types', ['yaml']);
+        $request->headers = new HeaderBag([
+            'Content-Type' => 'application/vnd.ibexa.api.ContentCreate+unknown',
+            'Accept' => 'application/vnd.ibexa.api.ContentCreate+unknown',
+        ]);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $this->expectException(UnsupportedMediaTypeHttpException::class);
+        $subscriber->allowOnlySupportedMediaTypes($event);
+    }
+
+    public function testThrowsExceptionWhenDifferentMediaTypesInHeadersAreUsed(): void
+    {
+        $request = new Request();
+        $request->attributes->set('supported_media_types', ['json']);
+        $request->headers = new HeaderBag([
+            'Content-Type' => 'application/vnd.ibexa.api.ContentCreate+json',
+            'Accept' => 'application/vnd.ibexa.api.ContentCreate+xml',
+        ]);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $this->expectException(UnsupportedMediaTypeHttpException::class);
+        $subscriber->allowOnlySupportedMediaTypes($event);
+    }
+
+    public function testApplicationJsonHeaderIsSupported(): void
+    {
+        $request = new Request();
+        $request->attributes->set('supported_media_types', ['json']);
+        $request->headers = new HeaderBag([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ]);
+
+        $subscriber = new SupportedMediaTypesSubscriber();
+        $event = new RequestEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $subscriber->allowOnlySupportedMediaTypes($event);
+
+        self::expectNotToPerformAssertions();
+    }
+}
